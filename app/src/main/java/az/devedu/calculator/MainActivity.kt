248768746559b75595aa303cdb4a8558f1298bde
Mirmohsun <mma@dbbsoftware.com>
2019-10-21 10:18:02 +0400

    Created calculator

---
+++ app/src/main/java/az/devedu/calculator/MainActivity.kt
@@ -0,0 +1,19 @@
+package az.devedu.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    public fun Addition (view: View){
+
+
+    }
+
+}
